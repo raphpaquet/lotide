6cb137cd7b178cd7ff171dd4966af3d1f1b20ede
Raphaelle Paquet <raphaelle.paquet@hotmail.com>
2020-10-28 22:01:38 +0000

    improvement of the function

--- eqArrays.js
+++ eqArrays.js
@@ -7,24 +7,26 @@ const assertEqual = function(actual, expected) {
   } return final;
 };
 
+
 const eqArrays = function(arr1, arr2) {
-  let result; 
+  if (arr1.length !== arr2.length) {
+    return false;
+  } 
   for (i = 0; i < arr1.length; i++) {
-    for (j = 0; j < arr2.length; j++) {
-      if (arr1[i] !== arr2[i]) {
-        result = false;
-      } else {
-        result = true;
-      }
-    }
-  } return result
-};
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    } 
+  } 
+  return true
+} 
 
 console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 4])) // => false
-
-
-console.log(assertEqual(eqArrays([1,2,3], [1,2,3]), true));
-console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 4]), true));
-
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+console.log(eqArrays(["1", "2", "3"], ["1", 2, "3"])) // => false*/
+console.log(eqArrays([2, 2, 5], [2, 2, '5'])); // => false
+
+
+console.log(assertEqual(eqArrays([1,2,3], [1,2,3]), true)); //Assertion Passed : true === true
+console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 4]), true)); // Assertion failed false !== true*/
+
