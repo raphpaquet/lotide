9dfbb805e0ed586674b5acbe349c20142d34a59e
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-01 17:49:37 +0000

    correction syntax

--- middle.js
+++ middle.js
@@ -2,18 +2,18 @@ const assertArraysEqual = require('./assertArraysEqual');
 
 
 const middle = function(arr) {
-  for (i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr.length === 1 || arr.length === 2) {
       return [];
     } else if (arr.length % 2 !== 0) {
       let index = Math.floor(arr.length / 2);
-      return [arr[index]]; 
+      return [arr[index]];
     } else {
-      let index1 = arr.length / 2
-      let index2 = index1 + 1
+      let index1 = arr.length / 2;
+      let index2 = index1 + 1;
       return [index1, index2];
     }
   }
-}
+};
 // console.log(middle([1,2,3,4,5]))
 // console.log(assertArraysEqual(middle([1,2,3,4,5]), [3]))
