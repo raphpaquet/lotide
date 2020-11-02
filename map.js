f9adda27f2b476d1547f0a4ed4db532930771fc8
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-02 23:02:47 +0000

    module export updated

--- map.js
+++ map.js
@@ -1,38 +1,24 @@
 // NEED TO TEST IT WITH ASSERT ARRAY EQUAL
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } 
-  for (i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    } 
-  } 
-  return true;
-} 
-
-const assertArraysEqual = function (arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log('💚💚 Passed')
-  } else {
-    console.log('❌❌ Failed')
-  } 
-}
+const assertArraysEqual = require('./assertArraysEqual')
+ 
 
 const words = ["ground", "control", "to", "major", "tom"];
-//const word2 = ['gris', 'cie', 'toi', 'maman', 'tata']
 
 
 const map = function(array, callback) {
-  const results = [];
+const results = [];
   for (let item of array) {
   results.push(callback(item));
   }
   return results;
 }
 
-const results1 = map(words, word => word[0]);
-console.log(results1);
+
+module.exports = map;
+
+// const results1 = map(words, word => word[0]);
+
+// const expected = ['g', 'c', 't', 'm', 't']
+// console.log(assertArraysEqual(results1, expected))
 
 
