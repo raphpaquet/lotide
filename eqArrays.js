95de5b4a6ba8d23db8a6eaf6b173ed3012c8dff7
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-02 20:31:42 +0000

    added console.log

--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,14 @@
-const assertEqual= require('./assertEqual')
-
-
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
-    return false;
-  } 
-  for (i = 0; i < arr1.length; i++) {
+    return console.log(false);
+  }
+  for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      return false;
-    } 
-  } 
-  return true
-} 
-
+      return console.log(false);
+    }
+  }
+  return console.log(true);
+};
 
 
 module.exports = eqArrays;
