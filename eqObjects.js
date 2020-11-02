3a06ad82d1a1056bc2df043ae9b1027124a618fe
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-02 22:59:47 +0000

    updated with module export

--- eqObjects.js
+++ eqObjects.js
@@ -1,22 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💚💚 Assertion Passed : ${actual} === ${expected}`)
-  } else {
-    console.log(`❌❌ Assertion failed : ${actual} !== ${expected}`)
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } 
-  for (i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    } 
-  } 
-  return true
-} 
+const assertEqual = require('./assertEqual')
+const eqArrays = require('./eqArrays')
 
 
 const eqObjects = function (object1, object2) {
@@ -28,13 +11,7 @@ let keys2 = Object.keys(object2) // return keys2: [ 'c', 'd' ]
 if (keys1.length !== keys2.length) {
   return false
 } else {
-// Iterate over value of object[key]
 for (let key of keys1) {
-    console.log('keys1 :', keys1)
-    console.log('key :' , key)
-    console.log('object1[key] :', object1[key]) 
-    console.log('object2[key] :', object2[key])  
-
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { 
       if (!eqArrays(object1[key], object2[key])) {
         return false
@@ -47,10 +24,10 @@ for (let key of keys1) {
 }
 
 
-const cd = { c: "1", d: ["2", 3]};
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
-//console.log(assertEqual(eqObjects(cd, cd2), false)); // => false
+// const cd = { c: "1", d: ["2", 3]};
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// console.log(eqObjects(cd, cd2)); // => false
+// console.log(assertEqual(eqObjects(cd, cd2), false)); // => false
 
 /*const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
