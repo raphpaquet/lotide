02429162620ad60046a86b0a7e6dfab6c018c9c3
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-02 20:41:08 +0000

    added console.log

--- middle.js
+++ middle.js
@@ -1,20 +1,18 @@
-const assertArraysEqual = require('./assertArraysEqual');
-
-
 const middle = function(arr) {
   for (let i = 0; i < arr.length; i++) {
     if (arr.length === 1 || arr.length === 2) {
-      return [];
+      return console.log([]);
     } else if (arr.length % 2 !== 0) {
       let index = Math.floor(arr.length / 2);
-      return [arr[index]];
+      return console.log([arr[index]]);
     } else {
       let index1 = arr.length / 2;
       let index2 = index1 + 1;
-      return [index1, index2];
+      return console.log([index1, index2]);
     }
   }
 };
+
 
 module.exports = middle;
 
