c1a8dd82810fa57982b0aebba5f75f10ab02544e
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-02 20:20:25 +0000

    added console.log for the returns

--- assertEqual.js
+++ assertEqual.js
@@ -1,15 +1,12 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return `💚💚 Assertion Passed : ${actual} === ${expected}`
+    console.log(`💚💚 Assertion Passed : ${actual} === ${expected}`)
   } else {
-    return `❌❌ Assertion failed : ${actual} !== ${expected}`
+    console.log(`❌❌ Assertion failed : ${actual} !== ${expected}`)
   }
 };
 
-// console.log(assertEqual('Lighthouse Labs', 'Bootcamp'))
-// console.log(assertEqual(1, 1));
-// assertEqual(1, 4);
-// assertEqual('Boot', 'Boot')
+
 
 
 module.exports = assertEqual; 
