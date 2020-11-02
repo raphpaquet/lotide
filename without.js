2f16cd6da33e155440d3edc36835e337a94e41fd
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-02 19:29:03 +0000

    refactor without function

---
+++ without.js
@@ -0,0 +1,15 @@
+const assertArraysEqual = require('./assertArraysEqual');
+
+
+ const without = function(source, itemsToRemove) {
+   let newArr = source;
+     for (let item of itemsToRemove) {
+       if(newArr.includes(item)) {
+        newArr.splice(newArr.indexOf(item), 1)
+       }
+   } return newArr;
+ }
+ 
+
+console.log(without([2, 3, 1], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
