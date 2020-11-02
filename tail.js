77c8bdab7df5d52c890d594e895a73a3e99bf3a6
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-02 18:31:35 +0000

    modularized code

--- tail.js
+++ tail.js
@@ -1,28 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💚💚 Assertion Passed : ${actual} === ${expected}`)
-  } else {
-    console.log(`🔴🔴 Assertion failed : ${actual} !== ${expected}`)
-  }
-};
-
-assertEqual('Lighthouse Labs', 'Bootcamp')
-assertEqual(1, 1);
-assertEqual(1, 4);
-assertEqual('Boot', 'Boot')
+const assertEqual = require('./assertEqual')
 
 
-
-function tail(array) {
-  return array.slice().splice(1, 2)
+const tail = function(array) {
+  return array.slice(1);
 }; 
 
+module.exports = tail;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); 
-assertEqual(words.length, 3);
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); 
