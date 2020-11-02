e2b6a90d71608bb1bc98818c3ab04f8a80ed1622
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-02 20:47:53 +0000

    added chai assert

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,5 @@
 const middle = require('../middle');
-const { assert } = require('chai');
+const assert = require('chai');
 
 describe('#middle', () => {
   it('shoud returns [3] for [1, 2, 3, 4, 5]', () => {
