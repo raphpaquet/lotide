93b633d5216ad875be7409c62a10959163981f48
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-02 20:39:21 +0000

    added chai testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,12 @@
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
+const { assert } = require('chai');
 
+describe('#middle', () => {
+  it('shoud returns [3] for [1, 2, 3, 4, 5]', () => {
+    assert.strictEqual(middle([1, 2, 3, 4, 5], [3]));
+  });
+});
 
-
-console.log(middle([1,2,3,4,5]))
-console.log(assertArraysEqual(middle([1,2,3,4,5]), [3]))
+// console.log(middle([1,2,3,4,5]))
+// console.log(assertArraysEqual(middle([1,2,3,4,5]), [3]))
