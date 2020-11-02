5b8bd80cfcae810f90a33196891d42cabfe72793
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-02 20:13:05 +0000

    added chai testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,14 +1,10 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+//const assertEqual = require('../assertEqual');
 
+describe('#tail', () => {
+  it('returns [2, 3, 4] for [1, 2, 3, 4]', () => {
+    assert.strictEqual(tail([1, 2, 3, 4], [2, 3, 4]))
+  })
+})
 
-
-const words = ["Yo Yo", "Lighthouse", "Labs", 'hi', 'allo'];
-console.log(tail(words)); 
-assertEqual(words.length, 3);
-
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-console.log(assertEqual(result.length, 2)); //ensure we get back two elements
-console.log(assertEqual(result[0], "Lighthouse")); //ensure first element is "Lighthouse"
-console.log(assertEqual(result[1], "Labs")); 
