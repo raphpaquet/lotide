06722c3522dccb4844007ca5b59db3595666e00b
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-02 23:00:12 +0000

    updated with module export

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,28 +1,22 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`💚💚 Assertion Passed : ${actual} === ${expected}`)
-  } else {
-    console.log(`❌❌ Assertion failed : ${actual} !== ${expected}`)
-  }
-}; 
+const assertEqual = require('./assertEqual');
 
 const findKeyByValue = function (obj, val) {
-  let keys = Object.keys(obj); // ['sci_fi', 'comedy', 'drama']
-  let result = '';
-  for (let key of keys) {
-    if (obj[key] === val) {
-        result = key;
-      }
-    } return result;
-  };
+  for (let key in obj) {
+    if(obj[key] === val) {
+      return key;
+    }
+  }
+};
 
-  const bestTVShowsByGenre = {
-    sci_fi: "The Expanse",
-    comedy: "Brooklyn Nine-Nine",
-    drama: "The Wire"
-  };
 
-  console.log(findKeyByValue(bestTVShowsByGenre, "The Wire")); // drama
-  console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show")); // undefined
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); 
+module.exports = findKeyByValue;
+  // const bestTVShowsByGenre = {
+  //   sci_fi: "The Expanse",
+  //   comedy: "Brooklyn Nine-Nine",
+  //   drama: "The Wire"
+  // };
+
+//   console.log(findKeyByValue(bestTVShowsByGenre, "The Wire")); // drama
+//   console.log(findKeyByValue(bestTVShowsByGenre, "That '70s Show")); // undefined
+// // assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// // assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined); 
