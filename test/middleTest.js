3858748acff492454c714821d4b3c4aa0b50d5e0
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-02 23:06:02 +0000

    updated last

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,9 @@
 const middle = require('../middle');
-const assert = require('chai');
+const assert = require('chai').assert;
 
 describe('#middle', () => {
   it('shoud returns [3] for [1, 2, 3, 4, 5]', () => {
-    assert.strictEqual(middle([1, 2, 3, 4, 5], [3]));
+    assert.deepEqual(middle([1, 2, 3, 4, 5], [3]));
   });
 });
 
