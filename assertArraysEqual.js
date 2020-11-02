ab05f0000ffc6c6f409ad1e4b506db65731cebaa
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-02 18:47:52 +0000

    modularized code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,27 +1,15 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } 
-  for (i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    } 
-  } 
-  return true;
-} 
+const eqArrays = require('./eqArrays')
+
 
 const assertArraysEqual = function (arr1, arr2) {
   if (eqArrays(arr1, arr2)) {
-    console.log('💚💚 Passed')
+    return '💚💚 Passed'
   } else {
-    console.log('❌❌ Failed')
+    return '❌❌ Failed'
   } 
 }
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // true
-assertArraysEqual(["2", 2, 5], [2, 2, 5]); //false
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) //false 
 
 
-
+module.exports = assertArraysEqual;
 
